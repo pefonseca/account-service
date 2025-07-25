3b5d75a74b46c14df6e0aab3832d3fc6569fa7bb
Pedro Fonseca <pedrofonunes@gmail.com>
2025-07-25 15:17:32 -0300

    feat: ajustando docker compose.

--- src/main/java/com/pefonseca/account/service/domain/producer/UserAccountEventProducer.java
+++ src/main/java/com/pefonseca/account/service/domain/producer/UserAccountEventProducer.java
@@ -3,6 +3,7 @@ package com.pefonseca.account.service.domain.producer;
 import com.pefonseca.account.service.domain.dto.general.UserAccountCreatedEventDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,13 @@ import org.springframework.stereotype.Service;
 public class UserAccountEventProducer {
 
     private final KafkaTemplate<String, UserAccountCreatedEventDTO> kafkaTemplate;
+
+    @Value("${kafka.topics.user-account-create}")
+    private String accountCreatedTopic;
+
+    public void publishUserAccountCreated(UserAccountCreatedEventDTO eventDTO) {
+        kafkaTemplate.send(accountCreatedTopic, eventDTO);
+        log.info("[UserAccountEventProducer] - (publishUserAccountCreated): event published: {}", eventDTO);
+    }
 
 }
